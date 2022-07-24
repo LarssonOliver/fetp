2df30f6dbf00728ad33ec6f3873a8e823e15097c
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-07-24 11:10:12 +0200

    Command reorder

--- src/command.rs
+++ src/command.rs
@@ -38,35 +38,24 @@ impl Command {
     }
 }
 
-fn extract_verb(buffer: &[u8]) -> Result<Verb, CommandError> {
-    let string = str::from_utf8(&buffer[..VERB_LENGTH]).unwrap();
-    match Verb::from_str(string) {
-        Ok(verb) => Ok(verb),
-        Err(err_str) => Err(CommandError(err_str)),
-    }
-}
-
-fn extract_argument(buffer: &[u8]) -> Vec<u8> {
-    let mut control_char_count = 0;
-    while buffer
-        .get(buffer.len() - control_char_count - 1)
-        .unwrap()
-        .is_ascii_control()
-    {
-        control_char_count += 1;
-    }
-
-    if buffer.len() <= VERB_LENGTH + control_char_count {
-        return Vec::new();
-    }
-
-    // + 1 is for the space between the verb and the argument.
-    buffer[VERB_LENGTH + 1..buffer.len() - control_char_count].to_vec()
-}
-
 fn validate_incoming_buffer(buffer: &[u8]) -> Result<(), CommandError> {
     validate_incoming_buffer_length(&buffer)?;
     validate_incoming_buffer_format(&buffer)?;
+    Ok(())
+}
+
+fn validate_incoming_buffer_length(buffer: &[u8]) -> Result<(), CommandError> {
+    if buffer.len() < VERB_LENGTH {
+        return Err(CommandError::default());
+    }
+
+    if buffer.len() > config::MAX_LINE_LENGTH {
+        return Err(CommandError(format!(
+            "Command too long, max length is {} bytes",
+            config::MAX_LINE_LENGTH
+        )));
+    }
+
     Ok(())
 }
 
@@ -94,19 +83,30 @@ fn utf8_buffer_to_string(buffer: &[u8]) -> Result<String, CommandError> {
     Ok(str::from_utf8(buffer).unwrap().to_string())
 }
 
-fn validate_incoming_buffer_length(buffer: &[u8]) -> Result<(), CommandError> {
-    if buffer.len() < VERB_LENGTH {
-        return Err(CommandError::default());
+fn extract_verb(buffer: &[u8]) -> Result<Verb, CommandError> {
+    let string = str::from_utf8(&buffer[..VERB_LENGTH]).unwrap();
+    match Verb::from_str(string) {
+        Ok(verb) => Ok(verb),
+        Err(err_str) => Err(CommandError(err_str)),
+    }
+}
+
+fn extract_argument(buffer: &[u8]) -> Vec<u8> {
+    let mut control_char_count = 0;
+    while buffer
+        .get(buffer.len() - control_char_count - 1)
+        .unwrap()
+        .is_ascii_control()
+    {
+        control_char_count += 1;
     }
 
-    if buffer.len() > config::MAX_LINE_LENGTH {
-        return Err(CommandError(format!(
-            "Command too long, max length is {} bytes",
-            config::MAX_LINE_LENGTH
-        )));
+    if buffer.len() <= VERB_LENGTH + control_char_count {
+        return Vec::new();
     }
 
-    Ok(())
+    // + 1 is for the space between the verb and the argument.
+    buffer[VERB_LENGTH + 1..buffer.len() - control_char_count].to_vec()
 }
 
 #[cfg(test)]
