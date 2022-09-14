bb631268de5229249f8c9868db70e6b2e54224dd
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-09-14 10:06:08 +0200

    Allo

--- src/command/executor/allo.rs
+++ src/command/executor/allo.rs
@@ -7,8 +7,27 @@ pub(crate) fn allo_command_executor(
     _argument: &str,
 ) -> Result<ExecutionResult, ExecutionError> {
     Ok(ExecutionResult {
-        status: 502,
-        message: "Command not implemented.".to_string(),
+        status: 202,
+        message: "Command not implemented, superfluous at this site.".to_string(),
         new_state: None,
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn always_accept() {
+        let state = SessionState::default();
+        for arg in ["", "foobar"] {
+            let res = allo_command_executor(&state, arg).unwrap();
+            assert_eq!(res.status, 202);
+            assert_eq!(
+                res.message,
+                "Command not implemented, superfluous at this site."
+            );
+            assert!(res.new_state.is_none());
+        }
+    }
 }
